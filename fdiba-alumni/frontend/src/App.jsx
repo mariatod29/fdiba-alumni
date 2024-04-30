8cf7155b8f54b9da1f24f1c2b0f3f17f4762573d
Maria Todorova <m.todorova@blubito.com>
2024-04-30 14:20:49 +0300

    feat: adjusted main home page design

--- fdiba-alumni/frontend/src/App.jsx
+++ fdiba-alumni/frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import HomePage from "./HomePage/HomePage";
+import HomePageWrapper from "./HomePage/HomePageWrapper";
 import RegistrationForm from "./RegistrationForm/RegistrationForm";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePageWrapper />} />
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/login" element={<div>Login Form Here</div>} />
         <Route path="/about" element={<div>About Us</div>} />
---
+++ fdiba-alumni/frontend/src/HomePage/HomePageFooter/HomePageFooter.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./HomePageFooter.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+export default function HomePageFooter() {
+  return (
+    <div className="footer">
+      <div className="emailContainer">
+        <p>
+          Contact us at:{" "}
+          <a href="mailto:contact@fdiba.tu-sofia.bg">
+            contact@fdiba.tu-sofia.bg
+          </a>
+        </p>
+      </div>
+      <div className="socialMediaContainer">
+        <p>Follow us on social media: </p>
+        <div className="socialMediaIcons">
+          <a href="https://www.facebook.com/FDIBA.TU.SOFIA">
+            <img src="/facebook-logo.png" alt="Facebook" className="icon" />
+          </a>
+          <a href="https://www.instagram.com/fdiba.tu.sofia/">
+            <img src="/instagram-logo.png" alt="Instagram" className="icon" />
+          </a>
+          <a href="https://www.linkedin.com/company/fdiba/">
+            <img src="/linkedin-logo.png" alt="LinkedIn" className="icon" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
