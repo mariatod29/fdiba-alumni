3b5ef27c9603c4e147a7858856528e575f7e122f
Maria Todorova <m.todorova@blubito.com>
2024-07-19 13:05:37 +0300

    fix: home page adjustments

--- fdiba-alumni/frontend/src/HomePage/HomePage/HomePage.jsx
+++ fdiba-alumni/frontend/src/HomePage/HomePage/HomePage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Carousel } from "primereact/carousel";
 import { Button } from "primereact/button";
 import "./HomePage.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
@@ -7,52 +6,16 @@ import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 
 export default function HomePage() {
-  const images = [
-    { url: "/fdiba-event.jpg", alt: "Event 1" },
-    { url: "/fdiba-event-2.jpg", alt: "Event 2" },
-    { url: "/fdiba-event-3.jpg", alt: "Event 3" },
-    { url: "/fdiba-event-4.jpg", alt: "Event 4" },
-  ];
-
-  const responsiveOptions = [
-    {
-      breakpoint: "1024px",
-      numVisible: 1,
-      numScroll: 1,
-    },
-    {
-      breakpoint: "768px",
-      numVisible: 1,
-      numScroll: 1,
-    },
-    {
-      breakpoint: "560px",
-      numVisible: 1,
-      numScroll: 1,
-    },
-  ];
-
-  const imageTemplate = (image) => {
-    return <img src={image.url} alt={image.alt} />;
-  };
-
   return (
     <div className="mainContainer">
-      <h1>Welcome to the Alumni Network!</h1>
-      <p>
-        This network helps you stay connected with fellow alumni and leverage
-        professional opportunities.
-      </p>
-      <Carousel
-        value={images}
-        numVisible={1}
-        numScroll={1}
-        responsiveOptions={responsiveOptions}
-        className="custom-carousel"
-        circular
-        autoplayInterval={5000}
-        itemTemplate={imageTemplate}
-      />
+      <div className="headline">
+        <h1>Welcome to the FDIBA Alumni Network!</h1>
+        <p className="subheadline">
+          This network helps you stay connected with fellow alumni and leverage
+          professional opportunities.
+        </p>
+      </div>
+
       <div className="buttons">
         <Button
           label="Register Now"
