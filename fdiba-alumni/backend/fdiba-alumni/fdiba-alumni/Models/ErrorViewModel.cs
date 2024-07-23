f15cc76c04067168e42ff74d24f13148350c507f
Maria Todorova <m.todorova@blubito.com>
2024-07-23 10:48:04 +0300

    fix: reverted backend commit

--- fdiba-alumni/backend/fdiba-alumni/fdiba-alumni/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace fdiba_alumni.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
